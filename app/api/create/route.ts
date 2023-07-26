5388d3ac92c6223293711b4eb2fa20ff6acfafdf
ian <ian.dev@pgpg.la>
2023-07-26 23:10:07 +0800

    add delete all json key

--- app/api/create/route.ts
+++ app/api/create/route.ts
@@ -1,4 +1,4 @@
-import { readFileSync, writeFileSync } from 'fs';
+import { readFileSync, writeFileSync, readdirSync } from 'fs';
 import { NextRequest, NextResponse } from 'next/server';
 import path from 'path';
 
@@ -20,7 +20,23 @@ export async function POST(req: NextRequest) {
 
 export async function DELETE(req: NextRequest) {
   const { key } = await req.json();
-  console.log(key)
-
-  return NextResponse.json({ isok: 'Y' });
+  try {
+    const dirPath = path.join(process.cwd(), `json`)
+    // 讀取指定目錄
+    let files = readdirSync(dirPath);
+    files.forEach(file => {
+      // 檢查檔案是否為 .json 文件
+      if (path.extname(file) === '.json') {
+        let content = JSON.parse(readFileSync(path.join(process.cwd(), 'json', `${file}`)).toString());
+        if (content.hasOwnProperty(key)) {  // 檢查有沒有key
+          delete content[key];
+          writeFileSync(path.join(process.cwd(), 'json', `${file}`), JSON.stringify(content));
+        }
+      }
+    });
+    return NextResponse.json({ isok: 'Y' });
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ message: 'Delete failed' });
+  }
 }
